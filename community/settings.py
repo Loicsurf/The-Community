02da3db8ddd1ffd1ab74e2153a936824511546ee
loicsurf <loicsurf20@gmail.com>
2021-04-18 10:19:11 +0100

    Fixed the main bug.
    Had a bug showing me "No module x found"

--- community/settings.py
+++ community/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATICFILES_STORAGE = 'community.storage.WhiteNoiseStaticFilesStorage'
-
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
 
