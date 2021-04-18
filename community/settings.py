d88cd15d7d6c454c6122de0170656f9e4f3ab418
loicsurf <loicsurf20@gmail.com>
2021-04-18 11:29:51 +0100

    Edited ALLOWEDHOST

--- community/settings.py
+++ community/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0#7go1v_dgm@1+=#q)-3aka5&i$!=@lcgef^ddujm=ybn6--_&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['serene-mountain-11159.herokuapp.com']
+ALLOWED_HOSTS = ['nesc-community.herokuapp.com']
 
 # Application definition
 
