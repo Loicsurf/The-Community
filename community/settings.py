c3d5f66a1e3a0d120913df3066d01bcc5a139a5e
loicsurf <loicsurf20@gmail.com>
2021-04-18 10:15:34 +0100

    Fixed bug

--- community/settings.py
+++ community/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATICFILES_STORAGE = 'The-community.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'community.storage.WhiteNoiseStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
