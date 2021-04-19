837410bb4d8bd040fe98b0602aeeebd7e74ce1d0
loicsurf <loicsurf20@gmail.com>
2021-04-19 10:12:16 +0100

    Changed Database
    I migrated all my data to postgresql than the local django database.

--- community/settings.py
+++ community/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'community.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'community',                      
+        'USER': 'loicsurf',
+        'PASSWORD': 'Ciacia144',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
